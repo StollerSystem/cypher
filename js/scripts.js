509fd10d0c4cf3242ae1822a97c65d9f2d97e8e3
Janet Karpenske <janetkarpenske@gmail.com>
2020-08-10 15:05:25 -0700

    Add third and fourth functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
-
-
-
+//2
 function capitalized(sentence) {
   let concatLetters = sentence.slice(0, 1).toUpperCase().concat(sentence.slice(-1).toUpperCase());
   let reversedLetters = letterReverse(concatLetters);
-
   return reversedLetters;
 }
-
+//3
 function letterReverse(concatLetters) {
   let letterArray = concatLetters.split("").reverse().join("");
   return letterArray;
 }
+//1
+function sentenceConcat(userInput) {
+  let newSentence = userInput.concat(capitalized(userInput))
+
+  let sentenceStart = sentenceDivide(userInput);
+  let finalSentence = sentenceStart.concat(newSentence);
+
+  return newSentence;
+}
+
+function sentenceDivide(userInput) {
+  let midLetterIndex = userInput.length / 2
+  let midLetterRound = parseInt(midLetterIndex);
+  let middleLetter = charAt(midLetterRound);
+  return middleLetter;
+}
 
 const userInput = prompt("Enter a sentence:");
-console.log(capitalized(userInput));
+console.log(sentenceConcat(userInput));
 });
